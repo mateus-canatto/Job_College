71990f324394090b5a37f91a6c8f30b97c46e9b6
mateus-canatto <mafaam01@gmail.com>
2026-03-23 08:32:15 -0300

    commit java calculadora

---
+++ java/Calculadora1.java
@@ -0,0 +1,38 @@
+// meu primeiro codigo em java
+
+public class Calculadora1 {
+    private double valor1;
+    private double valor2;
+    private double resultado;
+
+
+    public Calculadora1(double v1, double v2){
+        valor1 = v1;
+        valor2 = v2;
+        resultado = 0.0;
+    } 
+
+    public void somar(){
+        resultado = valor1 + valor2;
+    }
+
+    public void subtrair(){
+        resultado = valor1 - valor2;
+    }
+
+    public void mostrarResultado(){
+        System.out.println(resultado);
+    }
+
+    public static void main(String[] args){
+        Calculadora1 calc = new Calculadora1(10, 15);
+
+        System.out.println("Soma: ");
+        calc.somar();
+        calc.mostrarResultado();
+
+        System.out.println("Subtração: ");
+        calc.subtrair();
+        calc.mostrarResultado();
+    }
+}
