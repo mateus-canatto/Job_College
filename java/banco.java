29c31ad05204e9a2f59df742d8d43c76fdcd3425
mateus-canatto <mafaam01@gmail.com>
2026-03-23 10:56:40 -0300

    commit banco teste

--- java/banco.java
+++ java/banco.java
@@ -1,12 +1,40 @@
 public class banco{
     private double saldo;
-    private String nome;
+    private double saque;
+    private double deposito;
 
-    public saque(){
-
+    public banco(double c1, double c2, double c3){
+        saldo = c1;
+        saque = c2;
+        deposito = c3;
     }
 
-    public deposito(){
+    public void saque(){
+        saldo = saldo - saque;
+    }
+
+    public void deposito(){
+        saldo = saldo + deposito;
+    }
+
+    public void consultarSaldo(){
+        System.out.println(saldo);
+    }   
+
+    public static void main(String[] args){
+        banco conta1 = new banco(200, 200, 0);
+        banco conta2 = new banco(200, 0, 200);
+
+        System.out.println("Seu saldo é");
+        conta1.saque();
+        conta1.deposito();
+        conta1.consultarSaldo();
+  
+
+        System.out.println("Seu saldo é");
+        conta2.saque();
+        conta2.deposito();
+        conta2.consultarSaldo();
         
     }
 }
