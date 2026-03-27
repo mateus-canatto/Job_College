e8e22ad47b3238f643a7df7c6e688884233957e4
mateus-canatto <mafaam01@gmail.com>
2026-03-27 12:13:08 -0300

    commit python autenticação 2.0

--- python/autenticação.py
+++ python/autenticação.py
@@ -1,14 +1,24 @@
 # Atividade 27/03 python Sistema de Informação
-
+import time
 usuario = {
     "Mateus": {"nome": "Mateus", "senha": "1234"},
     "Gabriel": {"nome": "Gabriel", "senha": "4321"}
 }
 
-user = input("Usuario: ")
-password = input("Senha: ")
+erros_totais = 0
 
-if user in usuario and password == usuario[user]["senha"]:
-    print("Seja bem vindo", user)
-else:
-    print("Senha incorreta, tente novamente")
+while True:
+    user = input("Usuario: ")
+    password = input("Senha: ")
+    if user in usuario and password == usuario[user]["senha"]:
+        print("Seja bem vindo", user)
+        break
+    else:
+        erros_totais += 1
+        print("Senha incorreta, tente novamente")
+        
+        tempo_minutos = 3 * (6 ** ((erros_totais - 1) // 3))
+        tempo_segundos = tempo_minutos * 60
+        
+        print(f"Sua conta foi bloqueada por {tempo_minutos} minutos")
+        time.sleep(tempo_segundos)
